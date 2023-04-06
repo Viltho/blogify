fe4e34e7858cc6b2ae9e7785a1eafeb31184c7d8
viltho <abdullahabuhammed@gmail.com>
2023-04-07 00:26:37 +0300

    new

--- src/components/AboutUs/Team.js
+++ src/components/AboutUs/Team.js
@@ -12,7 +12,7 @@ function Team() {
   const obj2 = { id: 2, img: abdullah, name: "ABDULLA ABU HAMAD", FaceLink: "https://www.facebook.com/", GhLink: " https://github.com/Viltho", InLink: "https://linkedin.com/in/a-a-h" };
   const obj3 = { id: 5, img: mohammad, name: "MOHAMMAD ALGHANIM", FaceLink: "https://www.facebook.com/", GhLink: " https://github.com/mohammadAlghanim", InLink: " linkedin.com/in/mohammad-alghanim-923101204" };
   const obj4 = { id: 4, img: jana, name: "JANA ALMOMANI", FaceLink: "https://www.facebook.com", GhLink: "", InLink: "https://www.linkedin.com/in/jana-almomani-4a9232254" };
-  const obj5 = { id: 3, img: doha, name: "DOHA KHAMAISEH", FaceLink: "https://www.facebook.com/", GhLink: " https://github.com/DohaKhamaiseh", InLink: ": https://www.linkedin.com/in/%D8%B6%D8%AD%D9%89-%D8%AE%D9%85%D8%A7%D9%8A%D8%B3%D9%87-05a728254" };
+  const obj5 = { id: 3, img: doha, name: "DOHA KHAMAISEH", FaceLink: "https://www.facebook.com/", GhLink: " https://github.com/DohaKhamaiseh", InLink: "https://www.linkedin.com/in/%D8%B6%D8%AD%D9%89-%D8%AE%D9%85%D8%A7%D9%8A%D8%B3%D9%87-05a728254" };
   const obj6 = { id: 6, img: doha, name: "HUSAM ZABIAN", FaceLink: "https://www.facebook.com/", GhLink: "https://github.com/Husam-Zabian", InLink: "https://www.linkedin.com/in/husam-zabian-486069261" };
 
   const Arr = [obj1, obj2, obj3, obj4, obj5, obj6];
